06b1c7d973999d6fa88f0cdce1f77fd1a447283e
Peter Sideris <petersid2022@gmail.com>
2023-11-01 11:21:24 +0200

    add: got elf header bytes long way to go

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
-use crate::emulator::CPU;
-use goblin::elf::{ Elf, ProgramHeader };
+//use crate::emulator::CPU;
+use goblin::elf::Elf;
+use goblin::error::Result;
 use std::env;
 use std::fs;
 use std::process;
@@ -13,28 +14,49 @@ fn help() {
     Description:
     Specify the path to a RISC-V (RV32I) binary that you wish to emulate.";
     println!("{}", help);
+    process::exit(0);
 }
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut cpu = CPU::new();
-    let args: Vec<String> = env::args().collect();
+pub fn parse(buffer: &[u8]) -> Result<()> {
+    let elf = Elf::parse(&buffer)?;
 
+    for ph in elf.program_headers {
+        if ph.p_type == goblin::elf::program_header::PT_LOAD {
+            if ph.p_filesz > 0 {
+                let start = ph.p_offset as usize;
+                let end = (ph.p_offset + ph.p_filesz) as usize;
+
+                let bytes = buffer
+                    .get(start..end)
+                    .ok_or_else(|| goblin::error::Error::Malformed("Invalid range".to_string()))?;
+
+                for &byte in bytes {
+                    println!("{:#010b}", byte);
+                }
+            }
+        }
+    }
+    Ok(())
+}
+
+fn read_directory(dir: &str) -> Result<()> {
+    let buffer = fs::read(&dir)?;
+    parse(&buffer)?;
+    Ok(())
+}
+
+fn main() {
+    //let mut cpu = CPU::new();
+    let args: Vec<String> = env::args().collect();
     if args.len() != 2 {
         help();
-        process::exit(0);
     }
 
     let dir = &args[1];
-    let bytes = fs::read(&dir)?;
-    let binary = Elf::parse(&bytes)
-
-    //for (i, &byte) in bytes.iter().enumerate() {
-    //    println!("{:08b}", byte);
-    //    if (i + 1) % 4 == 0 {
-    //        println!();
-    //    }
-    //}
+    if let Err(err) = read_directory(&dir) {
+        eprintln!("Error reading directory: {}", err);
+        process::exit(0);
+    }
 
     //CPU::emulate_cycle(&mut cpu, out);
-    Ok(())
 }
